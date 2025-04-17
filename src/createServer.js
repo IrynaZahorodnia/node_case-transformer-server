48e6763df3ae1edaa48a1dde9dc62cd9d090261e
Iryna Zahorodnia <irishas25@ukr.net>
2025-04-17 13:10:25 +0300

    node-case

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,46 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { checkErrors } = require('./checkErrors');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const url = req.url.split('?');
+    const originalText = url[0].slice(1);
+    const params = new URLSearchParams(url[1]);
+    const toCase = params.get('toCase');
+    const errors = checkErrors(originalText, toCase);
+
+    if (errors.length) {
+      const errResult = {
+        errors,
+      };
+
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify(errResult));
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const { originalCase, convertedText } = convertToCase(originalText, toCase);
+    const result = {
+      originalCase,
+      targetCase: toCase,
+      originalText,
+      convertedText,
+    };
+
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
